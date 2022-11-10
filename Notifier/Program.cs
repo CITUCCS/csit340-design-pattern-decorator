1076bf0f3dc8296af98c221c2d8f769b3d9ec8f8
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-10 18:17:14 +0800

    Add sample

--- Notifier/Program.cs
+++ Notifier/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Notifier;
+
+Notification notification = new Notification();
+OnCriticalEvent(notification);
+
+notification = new FacebookNotification();
+OnCriticalEvent(notification);
+
+notification = new SmsNotification();
+OnCriticalEvent(notification);
+
+notification = new SlackNotification();
+OnCriticalEvent(notification);
+
+
+
+
+void OnCriticalEvent(Notification notification)
+{
+    const string message = "CRITICAL EVENT";
+    notification.SendMessage(message);
+}
---
+++ Notifier/SlackNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifier
+{
+    public class SlackNotification : Notification
+    {
+        public override void SendMessage(string message)
+        {
+            Console.WriteLine("Slack Notification: " + message);
+        }
+    }
+}
